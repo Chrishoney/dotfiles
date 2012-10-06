ed4d9b55fd6555272ab1bf4c7835a0b9a6d997ba
Chris Honey <chrishoney@xplroar.com>
2012-10-06 18:57:54 -0500

    Final changes to makelinks.py (for now).

--- makelinks.py
+++ makelinks.py
@@ -2,11 +2,10 @@
 """makelinks.py
 
 Usage:
-  makelinks.py (--wipe | --backup <ext>) <os>
+  makelinks.py [--backup <ext>] <os>
 
 Options:
   -h --help       Show this screen.
-  --wipe          Delete existing dotfiles
   --backup <ext>  Backup dotfiles as 'name' + 'ext'
 
 Arguments:
@@ -21,53 +20,52 @@ try:
 except ImportError:
     from lib.docopt import docopt
 
-LINK_PATH = os.environ['HOME']
-INCLUDE = (
-    ".bash_aliases",
-    ".bash_functions",
-    ".bash_profile",
-    ".bashcolors",
-    ".bashrc",
-    ".profile"
-)
+link_path = os.environ['HOME']
+platforms = {'mac': 'darwin',
+             'linux': 'linux2'}
 
-os_choices = ('linux', 'mac')
-dotfile_names = INCLUDE
-link_path = LINK_PATH
+def platform():
+    for platform in platforms.keys():
+        if platforms[platform] == sys.platform:
+            return os.listdir(platform)
+    return None
 
-def dotlinks(path=link_path):
+def file_names():
+    files = get_filenames()
+    if files:
+        return [os.path.join(os.getcwd(), dotfile) for dotfile in files]
+
+dotfile_names = file_names()
+
+def dotlinks(path):
     ''' Return a list of link destination paths '''
     return [os.path.join(path, fname) for fname in dotfile_names]
+
 
 def dotfiles(os_choice, path=None):
     ''' Return a list of dotfiles for the selected os '''
     path = os.getcwd()
+    # handle <os> argument
     if not os_choice in os.listdir(path):
         sys.exit('folder %s is missing' % os_choice)
     path = os.path.join(path, os_choice)
     return [os.path.join(path, fname) for fname in dotfile_names]
 
 def make_links(files):
-    ''' Create the symlinks, fail and exit if we can't write '''
+    ''' Create the symlinks. This will always succeed unless
+        $HOME isn't writable. '''
     for pair in files:
         try:
             os.symlink(*pair)
         except OSError:
-            sys.exit('Please run with --wipe or --backup')
+            pass
 
 def verify_links(files):
+    ''' Verify we created all the symlinks '''
     for link in files:
         if not os.path.islink(link):
             return False
     return True
-
-def wipe(files):
-    ''' Delete existing dotfiles, fail and exit if we can't write '''
-    for fname in files:
-        try:
-            os.unlink(os.path.join(link_path, fname))
-        except OSError:
-            pass
 
 def backup(files, ext):
     ''' Create a copy of fname at /path/to/fname.ext for each file '''
@@ -79,15 +77,23 @@ def backup(files, ext):
             dst = src + ext
             shutil.move(src, dst)
 
+def wipe(files):
+    ''' Delete existing dotfiles, fail and exit if we can't write '''
+    for fname in files:
+        try:
+            os.unlink(os.path.join(link_path, fname))
+        except OSError:
+            # pass if we can't unlink an imaginary file
+            pass
+
 if __name__ == '__main__':
     args = docopt(__doc__)
     links = dotlinks()
-    files = dotfiles(args['OS'])
-    # wipe or backup
+    files = dotfiles(args['<os>'])
+    # backup
     if args['--backup']:
         backup(links, args['--backup'])
-    elif args['--wipe']:
-        wipe(links)
+    wipe(links)
     # create the links
     make_links(zip(files, links))
     if verify_links(links):
